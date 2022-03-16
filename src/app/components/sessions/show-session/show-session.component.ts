b99fbc19c3fc061a099144dfbb7083bae0cc4662
Andelo Jurisic <ajurisic@pmfst.hr>
2022-03-16 21:22:59 +0100

    Ispis

--- src/app/components/sessions/show-session/show-session.component.ts
+++ src/app/components/sessions/show-session/show-session.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { SessionApiService } from 'src/app/session-api.service';
 @Component({
   selector: 'app-show-session',
   templateUrl: './show-session.component.html',
   styleUrls: ['./show-session.component.css']
 })
 export class ShowSessionComponent implements OnInit {
-
-  constructor() { }
+  sessionList$!:Observable<any[]>;
+  
+  constructor(private service:SessionApiService) { 
+    
+  }
 
   ngOnInit(): void {
+    this.sessionList$=this.service.getSessionList();
+
   }
 
 }
